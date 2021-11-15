8d44e67c7132932719e9c23fbefb361e4279adbb
James Keveren <james@keve.ren>
2021-11-15 11:33:09 +0000

    random order and configurable out file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <fstream>
 #include <filesystem>
 #include <sys/xattr.h>
+#include <algorithm>
+#include <random>
 #include <string.h>
 #include <Magick++.h>
 
@@ -11,38 +12,44 @@ using namespace Magick;
 int main(int argc, char *argv[]) {
 	InitializeMagick("");
 
-	if (argc <= 2) {
-		cerr << "Usage: crop-collage <base-dir> <xattr name>\n";
+	if (argc <= 3) {
+		cerr << "Usage: crop-collage <base directory> <xattr name> <out file>\n";
 		return 1;
 	}
 
 	string baseDirName = argv[1];
-	string xattrName = argv[2];
+	char *xattrName = argv[2];
+	string outFileName = argv[3];
 
-	printf("Looking for files in \"%s\" with xattr \"%s\"\n", baseDirName.c_str(), xattrName.c_str());
+	printf("Looking for files in \"%s\" with xattr \"%s\" adnd and saving to \"%s\"\n", baseDirName.c_str(), xattrName, outFileName.c_str());
 
-	list<Image> images;
+	vector<Image> images;
 	Image image;
 	int bufferLength = 100;
 	for (auto& p: std::filesystem::recursive_directory_iterator(baseDirName)) {
 		string fileName = p.path();
 
 		char cValue[bufferLength];
-		ssize_t size = getxattr(fileName.c_str(), xattrName.c_str(), cValue, bufferLength);
+		ssize_t size = getxattr(fileName.c_str(), xattrName, cValue, bufferLength);
 		if (size == -1) {
 			continue;
 		}
+		cerr << "Processing: " << fileName << endl;
 		string geometrySpec(cValue);
 		geometrySpec = geometrySpec.substr(0, size);
-		cout << fileName << endl;
 		
 		image.read(fileName);
 		image.crop(Geometry(geometrySpec));
 		image.scale(Geometry(430, 0));
 		images.push_back(image);
 	}
+
+	unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+	default_random_engine randomEngine(seed);
+	    
+	shuffle(begin(images), end(images), randomEngine);
 	
-	list<Image> montage;
+	vector<Image> montage;
 	
 	Montage montageOptions;
 	montageOptions.geometry(Geometry(0,0));
@@ -50,7 +57,8 @@ int main(int argc, char *argv[]) {
 	montageOptions.backgroundColor(ColorGray(0.5));
 	
 	montageImages(&montage, images.begin(), images.end(), montageOptions);
-	writeImages(montage.begin(), montage.end(), "out.jpg");
+	
+	writeImages(montage.begin(), montage.end(), outFileName);
 
 	return 0;
 }
